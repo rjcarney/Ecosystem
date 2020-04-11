b1b0e4f851e4daeae18d6721e423a089b7b6e96b
rjcarney <33989142+rjcarney@users.noreply.github.com>
2020-04-10 21:23:33 -0400

    Instantiate Blank Tiles
    Create a grid of blank tiles.

---
+++ Ecosystem/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private MapGenerator mapGen;
+    public int mapWidth;
+    public int mapHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mapGen = gameObject.GetComponent<MapGenerator>();
+        mapGen.CreateMap(mapWidth, mapHeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Ecosystem/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    // Size of map
+    private int width;
+    private int height;
+
+    public GameObject tile;
+
+    public void CreateMap(int width, int height) {
+        this.width = width;
+        this.height = height;
+
+        for (int x = 0; x < this.width; x++) {
+            for (int z = 0; z < this.height; z++) {
+                Instantiate(tile, new Vector3(x*2, 0, z*2), Quaternion.identity);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
